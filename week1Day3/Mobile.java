f9ebdfa167ebd4e7accff400ef554f09d3281a00
Dhanush1006 <dhanushkannaiyan123@gmail.com>
2024-08-22 03:35:55 -0700

    Add files via upload
    day1

---
+++ week1Day3/Mobile.java
@@ -0,0 +1,30 @@
+package week1Day3;
+
+public class Mobile {
+
+	public static void main(String[] args) {
+
+		String mobilename = "Oppo";
+		String model = "f11";
+		int price = 20000;
+		float androidversion = 10.1f;
+		double mobilesarnumber = 14356.5436784;
+		char typeofcable = 'C';
+		boolean ischargeravailable = true;
+		long imeNumber = 6754893089064345l;
+
+		System.out.println("mobilename");
+		System.out.println("model");
+		System.out.println("price");
+		System.out.println("androidversion");
+		System.out.println("mobilesarnumber");
+		System.out.println("typeofcable");
+		System.out.println("ischargeravailable");
+		System.out.println("imeNumber");
+
+		System.out.println(mobilename + "\n" + model + "\n" + price + "\n" + androidversion + "\n" + mobilesarnumber
+				+ "\n" + typeofcable + "\n" + ischargeravailable + "\n" + imeNumber);
+
+	}
+
+}
